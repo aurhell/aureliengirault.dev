f7af831ea4bcb9937c4a9324fadb9a6a78f588c4
Aurélien Girault <me@agirault.com>
2024-02-20 09:15:21 +0100

    :recycle: Refactor(MainPage): update animation name

--- src/App.tsx
+++ src/App.tsx
@@ -42,8 +42,8 @@ function Face() {
 function Name() {
   return (
     <motion.div
-      initial={{ y: -800, opacity: 0, scale: 4 }}
-      animate={{ y: 0, opacity: 1, scale: 1 }}
+      initial={{ y: -800, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
       transition={{
         duration: 0.8,
         delay: 0.4,
